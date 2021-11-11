4a58789034ccf99aebaa4541d2a6be225571338b
Kateryna Dmytriieva <katuxadmitr@gmail.com>
2021-11-11 14:50:56 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,25 +2,12 @@ import VueSelect from './components/Select.vue'
 import mixins from './mixins/index'
 
 
- */
 const _componentName = 'VueSelect'
-/**
- * @description Support create multiple vfm instance
- */
 
-/**
- * @description Register vfm instance globally
- * @deprecated not available in vue-final-modal 4
- */
 const installVfm = (Vue, options = {}) => {
  
   Vue.component(_componentName, VueSelect)
 }
-
-/**
- * @description Vue plugin for register vfm instance globally
- * @deprecated not available in vue-final-modal 4
- */
 export const vfmPlugin = pluginOptions => ({
   install(Vue, options) {
     const _options = Object.assign({}, pluginOptions, options)
